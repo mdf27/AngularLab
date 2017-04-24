604af3f1086f2118adf4d10291f7d5947079c5e5
dani_fag <danifagda@gmail.com>
2017-04-23 23:47:09 -0300

    added movie details page

--- resources/node-server/public/app.js
+++ resources/node-server/public/app.js
@@ -1,31 +1,56 @@
-var moviecatApp = angular.module('moviecatApp',[]);
+var moviecatApp = angular.module('moviecatApp',['ngRoute']);
+
+moviecatApp.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		controller: 'maincontroller',
+		templateUrl:'Partials/main.html'		
+	})
+	.when('/movieDetails',{
+		controller: 'movieDetailsController',
+		templateUrl:'Partials/movieDetails.html'		
+	})
+    .otherwise({
+        redirectTo: '/'
+    });
+})
 
 moviecatApp.factory('movieFactory', function($http){
-  var factory = {};
+  var factory = {
+  	currentMovie : ''
+  };
   factory.getAll = function(data){
     $http.get('/api/movies').then(data);    
+  }; 
+  factory.movieDetails = function(movie, data){
+  	$http.get('/api/movies/'+movie.id)
+  	.then(data);
   };  
+  
   return factory;
 });
-
-moviecatApp.controller('maincontroller', ['$scope', 'movieFactory', maincontroller]);
 
 function maincontroller($scope, movieFactory) {
 
 	movieFactory.getAll(function(result){
-    $scope.movies = result.data;
-  });
+    	$scope.movies = result.data;
+  	});
 
-    $scope.addCity = function() {
-    	/*var newCity = {
-    			name: $scope.newCity,
-    			country: $scope.newCountry
-    		}
-    	
-    	cityFactory.addCity(newCity);
-    	$scope.newCity= '';
-    	$scope.newCountry= '';*/
-    }
+  	$scope.currentMovie = function(movie){
+  		movieFactory.currentMovie = movie;		
+	}
+	
 }
 
+moviecatApp.controller('maincontroller', ['$scope', 'movieFactory', maincontroller]);
+
+moviecatApp.controller('movieDetailsController', ['$scope', 'movieFactory', movieDetailsController]);
+
+function movieDetailsController($scope, movieFactory) {
+
+	movieFactory.movieDetails(movieFactory.currentMovie, function(result){
+			$scope.movieDetails = result.data;
+	});	
+
+}
 
