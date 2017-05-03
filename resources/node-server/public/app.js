3622ba47935aea29e07905e59e01cdb6bd9c2ca9
dani_fag <danifagda@gmail.com>
2017-05-02 23:39:57 -0300

    fixes in loader

--- resources/node-server/public/app.js
+++ resources/node-server/public/app.js
@@ -54,3 +54,26 @@ function movieDetailsController($scope, $routeParams, movieService) {
 	});	
 
 }
+
+moviecatApp.directive('loader',['$http' ,function ($http)
+    {
+        return {
+            restrict: 'E',
+            link: function ($scope, element, attrs)
+            {
+                $scope.isLoading = function () {
+                    return $http.pendingRequests.length > 0;
+                };
+
+                $scope.$watch($scope.isLoading, function (value)
+                {
+                    if(value){
+                        $(element).show();
+                    }else{
+                        $(element).hide();
+                    }
+                });
+            },
+            templateUrl: 'templates/loader-template.html'
+        };
+}]);
