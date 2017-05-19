8ec6cca9dfa2173340581f3a462bedeb03e05d80
dani_fag <danifagda@gmail.com>
2017-05-18 23:42:54 -0300

    added button bag

--- resources/node-server/public/pages/movieDetails/movieDetails.controller.js
+++ resources/node-server/public/pages/movieDetails/movieDetails.controller.js
@@ -1,11 +1,12 @@
 define([], function(){
 	function movieDetailsController($scope, $routeParams, movieService) {
-	  $scope.routeParams = $routeParams;
+		$scope.routeParams = $routeParams;
 		movieService.movieDetails($routeParams.movieId, function(result){
 				$scope.movieDetails = result.data;
 		});	
+		$scope.addToBag = function addToBag() {
+			console.log("hello");
+		}
 	}
-
-
 	return movieDetailsController;
 });
